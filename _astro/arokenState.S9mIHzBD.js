1991b78b0786edfedcb2fe332d027ac935acb41b
ThunDer <thunder00104@gmail.com>
2025-11-29 14:40:54 +0200

    Updates

---
+++ _astro/arokenState.S9mIHzBD.js
@@ -0,0 +1,1 @@
+function n(e){typeof window<"u"&&window.dispatchEvent(new CustomEvent("aroken-trigger",{detail:e||{}}))}export{n as t};
---
+++ _astro/index.astro_astro_type_script_index_1_lang.BvUqKnS3.js
@@ -0,0 +1,1 @@
+import{t as e}from"./arokenState.S9mIHzBD.js";document.getElementById("test-aroken")?.addEventListener("click",()=>{e()});
