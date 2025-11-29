77c15cee60608d5a82b1151cebe0aa17044ac5ea
ThunDer <thunder00104@gmail.com>
2025-11-29 20:24:40 +0200

    Clear cfg

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://v1rtezz.github.io",
-  base: "/aroken_event/", 
-});
+export default defineConfig({});
